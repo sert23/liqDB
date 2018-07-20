9fc0026c7f27ad494027678bda319761ed63d8f8
eaparicio45 <erapariciopuerta@hotmail.com>
2018-07-20 18:39:36 +0200

    adding MEDIA_URL

--- gentelella/compare/views.py
+++ gentelella/compare/views.py
@@ -6,7 +6,7 @@ import json
 import os
 from compare.forms import CompareForm
 from django.core.urlresolvers import reverse_lazy
-from gentelella.settings import BASE_DIR, DATA_FOLDER, MEDIA_ROOT, SUB_SITE
+from gentelella.settings import BASE_DIR, DATA_FOLDER, MEDIA_ROOT, SUB_SITE, MEDIA_URL
 from study.views import sortedMatrixToTableList
 from study.summary_plots import makeGenomePlot, makeTop20, makePie10,makeSpeciesPlot,makeTop20CV,makeBottom20CV,makeDEbox
 # Create your views here.
