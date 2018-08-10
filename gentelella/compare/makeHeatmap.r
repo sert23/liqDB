c7505d960fc49492a2477895f6ab24c49e31aa2a
eaparicio45 <erapariciopuerta@hotmail.com>
2018-08-10 15:30:45 +0200

    changin legend

--- gentelella/compare/makeHeatmap.r
+++ gentelella/compare/makeHeatmap.r
@@ -24,7 +24,7 @@ print("you")
 
 #heatmaply(head(log_trans,20), colors = RdYlGn,  file = "/home/eap/heatmaply_plot30.html")
 heatmaply(head(log_trans,20), colors = c("Red", "Black", "Green"),
-file = paste(input_folder,"heatmap_euclidean.html",sep ="/"), fontsize_col=8, column_text_angle=60)
+file = paste(input_folder,"heatmap_euclidean.html",sep ="/"), fontsize_col=8, column_text_angle=60, key.title="log(RPM+1)")
 
 
 
