31ee8818f435237cffd80c451313de97e9b84024
eaparicio45 <erapariciopuerta@hotmail.com>
2018-08-12 00:18:07 +0200

    adding group names

--- gentelella/compare/makeHeatmap.r
+++ gentelella/compare/makeHeatmap.r
@@ -6,27 +6,21 @@ args <- commandArgs(TRUE)
 #load DE genes
 #input_folder<- "/opt/liqDB/liqDB/gentelella/data_folder/queryData/JPCLAFL8UHJMRJLKEQSZ/queryOutput/de/Groups"
 #input_folder<- "C:/Users/Ernesto/PycharmProjects/liqDB/gentelella/data_folder/queryData/JPCLAFL8UHJMRJLKEQSZ/queryOutput/de/Groups"
-#input_folder <- "C:/Users/Ernesto/Desktop/queryOutput/de/Groups"
 input_folder<- args[1]
-print("hey")
 
-DE_file <- paste(input_folder,"de_miRNA_RPMadjLib_Group1-Group2.txt", sep= "/")
-DE_table <- read.delim(DE_file,  stringsAsFactors=FALSE, header = TRUE)
 
-DE_table <- DE_table[order(-DE_table$FC),]
-DE_table <- head(DE_table,20)
+DE_file <- paste(input_folder,"matrix_miRNA_RPMadjLib.txt", sep= "/")
+DE_table <- read.delim(DE_file,  stringsAsFactors=FALSE, header = FALSE)
 row_vector <- DE_table[,1]
 gene_names<-unique(gsub("#.+", "", row_vector))
 
 #load exp_mat
-exp_file<- paste(gsub("\\/de\\/.+","",input_folder),"/miRNA_RPMadjLib_sort.txt", sep="")
-
-exp_mat <- read.delim(exp_file, row.names=1, stringsAsFactors=FALSE)
-exp_mat$sum <- NULL
+exp_file<- paste(input_folder,"matrix_sort_annot.txt", sep= "/")
+#exp_file<- paste(gsub("\\/de\\/.+","",input_folder),"/miRNA_RPMadjLib.txt", sep="")
+exp_mat <- read.delim(exp_file, row.names=1, stringsAsFactors=FALSE,  comment.char = "")
 x <- subset(exp_mat, rownames(exp_mat) %in% gene_names)
 log_trans <- log(x+1)
 
-print("you")
 
 #heatmaply(head(log_trans,20), colors = RdYlGn,  file = "/home/eap/heatmaply_plot30.html")
 heatmaply(head(log_trans,20), colors = c("Red", "Black", "Green"),
