de2c6a9d8bb2ff6314cb54ca993d3a3308065aae
eaparicio45 <erapariciopuerta@hotmail.com>
2018-08-12 00:45:11 +0200

    adding cellnotes

--- gentelella/compare/makeHeatmap.r
+++ gentelella/compare/makeHeatmap.r
@@ -25,7 +25,7 @@ log_trans <- log(x+1)
 #heatmaply(head(log_trans,20), colors = RdYlGn,  file = "/home/eap/heatmaply_plot30.html")
 heatmaply(head(log_trans,20), colors = c("Red", "Black", "Green"),
           file = paste(input_folder,"heatmap_euclidean.html",sep ="/"),
-          fontsize_col=8, column_text_angle=60, key.title="log(RPM+1)", cellnote = x)
+          fontsize_col=8, column_text_angle=60, key.title="log(RPM+1)", cellnote = x, draw_cellnote=FALSE)
 
 
 
