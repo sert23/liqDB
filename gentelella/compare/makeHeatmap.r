593111ef8764b2d8f251ae28a66e2db6429cd260
eaparicio45 <erapariciopuerta@hotmail.com>
2018-08-12 01:46:23 +0200

    bug

--- gentelella/compare/makeHeatmap.r
+++ gentelella/compare/makeHeatmap.r
@@ -27,7 +27,8 @@ log_trans <- log(x+1)
 heatmaply(head(log_trans,20), colors = c("Red", "Black", "Green"),
           file = paste(input_folder,"heatmap_euclidean.html",sep ="/"),
           fontsize_col=8, column_text_angle=60, key.title="log(RPM+1)", custom_hovertext = x,
-          ColSideColors = gsub(".+\\.", "", colnames(x)) )
+          col_side_colors = gsub(".+\\.", "", colnames(x)) )
+          #ColSideColors = gsub(".+\\.", "", colnames(x)) )
           #\code{function(...) round(..., digits=3, )
 
 
