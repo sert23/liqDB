15d43c70dd1f0abd2424de1eb096861639d9c5c6
eaparicio45 <erapariciopuerta@hotmail.com>
2018-09-21 19:14:47 +0200

    fixing request not defined

--- gentelella/samples/views.py
+++ gentelella/samples/views.py
@@ -189,7 +189,7 @@ class SampleQuery(FormView):
     template_name = 'app/samples_query.html'
     form_class = SamplesForm
     #post_dict = HttpRequest.POST
-    post_dict = request.POST
+    post_dict = HttpRequest.POST
     import json
     with open('/opt/liqDB/liqDB/gentelella/queryData/post.json', 'w') as fp:
         json.dump(post_dict, fp)
