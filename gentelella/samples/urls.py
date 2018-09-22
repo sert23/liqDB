9c79eddaaac26c63d6450e5e12fbed6e6b7888d6
eaparicio45 <erapariciopuerta@hotmail.com>
2018-09-22 16:53:13 +0200

    adding link for compare 2 datasets

--- gentelella/samples/urls.py
+++ gentelella/samples/urls.py
@@ -4,8 +4,10 @@ from django.conf.urls import url
 
 urlpatterns = [
     #url(r'(<SRP>[A-Za-z0-9]+)', views.DisplayStudy.as_view())
-    url(r'^(?P<query_id>[A-za-z0-9]+)', views.SampleQuery.as_view()),
     url(r'^(pick/[A-za-z0-9]+)', views.PickSample.as_view()),
+
+    url(r'^(?P<query_id>[A-za-z0-9]+)', views.SampleQuery.as_view()),
+
     url(r'^', views.StartSample.as_view(),name="samples")
     # url(r'^', views.samples_table)
 ]
