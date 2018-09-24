7e75e98757904081f833a0eb5ededb4644905ffe
eaparicio45 <erapariciopuerta@hotmail.com>
2018-09-24 18:37:51 +0200

    adding DE_file for DEplot

--- gentelella/compare/views.py
+++ gentelella/compare/views.py
@@ -8,7 +8,7 @@ from compare.forms import CompareForm
 from django.core.urlresolvers import reverse_lazy
 from gentelella.settings import BASE_DIR, DATA_FOLDER, MEDIA_ROOT, SUB_SITE, MEDIA_URL, PATH_TO_RSCRIPT, HM_SCRIPT
 from study.views import sortedMatrixToTableList
-from study.summary_plots import makeGenomePlot, makeTop20, makePie10,makeSpeciesPlot,makeTop20CV,makeBottom20CV,makeDEbox
+from study.summary_plots import makeGenomePlot, makeTop20, makePie10,makeSpeciesPlot,makeTop20CV,makeBottom20CV,makeDEbox,makeDEbox_pval
 import subprocess
 # Create your views here.
 
@@ -144,14 +144,23 @@ class CompareQueries(TemplateView):
                     os.path.join(content_folder, "de", comparison, "matrix_miRNA_RPMadjLib.txt").replace(
                             "\\", "/")):
                 DE_table = os.path.join(content_folder, "de", comparison, "").replace("\\", "/")
-                DE_plot = makeDEbox(
-                    os.path.join(content_folder, "de", comparison, "matrix_miRNA_RPMadjLib.txt")).replace(
-                    "\\", "/")
+                # DE_plot = makeDEbox(
+                #     os.path.join(content_folder, "de", comparison, "matrix_miRNA_RPMadjLib.txt")).replace(
+                #     "\\", "/")
+
+                DE_file = [f for f in files if f.startswith("de_")][0]
+
+                DE_plot = makeDEbox_pval(os.path.join(content_folder, "de", comparison, "matrix_miRNA_RPMadjLib.txt"),
+                                         os.path.join(content_folder, "de", comparison, DE_file))
+
                 HM_path = os.path.join(MEDIA_URL,  "queryData",query_id, "queryOutput", "de", comparison, "heatmap_euclidean.html")
                 HM_link = "<a href='" + HM_path + "'><h3><b> See heatmap with hierarchical clustering </b><h3></a>"
 
                 if not os.path.exists(os.path.join(MEDIA_ROOT,  "queryData",query_id, "queryOutput","de",comparison,"heatmap_euclidean.html")):
-                    subprocess.Popen([PATH_TO_RSCRIPT, HM_SCRIPT, os.path.join(MEDIA_ROOT,  "queryData",query_id, "queryOutput","de",comparison), ">",os.path.join(MEDIA_ROOT,  "queryData",query_id, "queryOutput","de",comparison,"Rlog.txt")])
+                    #output = ('temp.txt', 'w') as output:
+                    #server = subprocess.Popen('./server.py', stdout=output)
+                    #subprocess.Popen([PATH_TO_RSCRIPT, HM_SCRIPT, os.path.join(MEDIA_ROOT,  "queryData",query_id, "queryOutput","de",comparison)], ">",os.path.join(MEDIA_ROOT,  "queryData",query_id, "queryOutput","de",comparison,"Rlog.txt")])
+                    print("lelo")
                 DE_objs.append([comparison, DE_table, DE_plot, HM_link])
             else:
                 DE_table = os.path.join(content_folder, "de", comparison, "").replace("\\", "/")
