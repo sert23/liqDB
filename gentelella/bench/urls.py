be5d5b806f3212647863fd0b81d87b9996cf873c
eaparicio45 <erapariciopuerta@hotmail.com>
2018-09-25 14:20:04 +0200

    updating urls

--- gentelella/bench/urls.py
+++ gentelella/bench/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 
 urlpatterns = [
     #url(r'(<SRP>[A-Za-z0-9]+)', views.DisplayStudy.as_view())
-
+    url(r'^(pick/[A-za-z0-9]+)', views.PickBench.as_view()),
     # url(r'^compare/(?P<query_id>[A-za-z0-9]+)', views.BenchCompare.as_view()),
     url(r'^compare/(?P<query_id>[A-za-z0-9]+)', views.bench),
     url(r'^(?P<query_id>[A-za-z0-9]+)', views.BenchSample.as_view()),
