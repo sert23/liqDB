e8d95d13780bafe5c98974fa6c81e20998686eaf
eaparicio45 <erapariciopuerta@hotmail.com>
2018-09-25 14:17:20 +0200

    adding "pick" to BENCH

--- gentelella/bench/forms.py
+++ gentelella/bench/forms.py
@@ -162,3 +162,88 @@ class BenchForm(forms.Form):
     def start_DE(self, old_query_id):
         query_id = self.generate_id()
         return self.make_DE(self.cleaned_data,query_id, old_query_id)
+
+class ManualForm(forms.Form):
+
+    hiddenIDs = forms.CharField(label='', required=False, widget=forms.HiddenInput, max_length=1000000)
+
+    #field2=  forms.CharField(label=')', required=False)
+
+    ##choices go here
+    def __init__(self, *args, **kwargs):
+        super(ManualForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Div(
+                Field('hiddenIDs', name='hiddenIDs')),
+                #Field('library', wrapper_class='col-md-2',css_class='form-control'),
+                FormActions(
+                # Submit('submit', 'RUN', css_class='btn btn-primary', onclick="alert('Neat!'); return true")
+                    #Submit('submit', 'KEEP SELECTED', onclick="$('#loadpage').show(); $('#divPageContent').hide();", css_class='btn btn-primary btn-form')
+                    Submit('submit', 'KEEP SELECTED SAMPLES', onclick = "keepSelected()", css_class='btn btn-primary btn-form'),
+
+                    Submit('submit', 'REMOVE SELECTED SAMPLES', onclick="removeSelected()", css_class='btn btn-primary btn-form'),
+
+                    Submit('submit', 'PROCEED WITH ALL SAMPLES', onclick="$('#loadpage').show(); $('#divPageContent').hide();proceed()", css_class='btn btn-primary btn-form'),
+
+                )
+
+        )
+
+    def generate_id(self):
+        is_new = True
+        while is_new:
+            query_id = generate_uniq_id()
+            #query_path =os.path.join(MEDIA_ROOT,query_id)
+            query_path =os.path.join(DATA_FOLDER,"queryData",query_id)
+            if not os.path.exists(query_path):
+                os.mkdir(query_path)
+                return query_id
+
+    def make_query(self,cleaned_data,query_id,old_query):
+
+
+        hiddenString = str(cleaned_data.get("hiddenIDs"))
+        hiddenList = hiddenString.split(",")
+        if hiddenList[-1] == "keep":
+            cleanList = [x for x in hiddenList if x not in ["keep", "proceed","remove"]]
+            queryString = ",".join(cleanList)
+            success_url = reverse_lazy("samples") + "pick/" + query_id
+
+        if hiddenList[-1] == "remove":
+            removeString = ",".join(hiddenList[:-1])
+            removeList = removeString.split(",")
+            success_url = reverse_lazy("samples") + "pick/" + query_id
+            with open(os.path.join(DATA_FOLDER,"queryData",old_query,"query.txt"), 'r') as queryfile:
+                old_SRX_string = queryfile.read()
+            old_list = old_SRX_string.split(",")
+            new_list = [x for x in old_list if x not in removeList]
+            cleanList = [x for x in new_list if x not in ["keep", "proceed", "remove"]]
+            queryString = ",".join(cleanList)
+
+        if hiddenList[-1] == "proceed":
+            success_url = reverse_lazy("samples") + query_id
+            with open(os.path.join(DATA_FOLDER, "queryData", old_query, "query.txt"), 'r') as queryfile:
+                queryString = queryfile.read()
+
+
+        #print(len(querySamples))
+        #samples_ids = samples.values_list('Experiment',flat=True)
+        #print(samples_ids)
+        #print(len(samples_ids))
+
+        query_path = os.path.join(DATA_FOLDER,"queryData", query_id)
+        outputPath = os.path.join(query_path,"queryOutput")
+
+        call = "java -jar /opt/sRNAtoolboxDB/exec/liqDB.jar output={outputPath} mode=matrix sampleString={sampleString}".format(
+            outputPath=outputPath,
+            sampleString=queryString
+        )
+
+        with open(os.path.join(query_path,"query.txt"), "w") as text_file:
+            text_file.write(queryString)
+        #print(query_id,fluid,sex,healthy,extraction,library)
+        return(query_id,call,success_url)
+    def start_query(self,old_query):
+        query_id = self.generate_id()
+        return self.make_query(self.cleaned_data,query_id, old_query)
--- gentelella/bench/views.py
+++ gentelella/bench/views.py
@@ -6,7 +6,7 @@ from django.views.generic import DetailView
 from gentelella.settings import BASE_DIR, DATA_FOLDER, MEDIA_ROOT, MICROS_FOLDER, SUB_SITE
 import os
 from study.summary_plots import makeMirBox
-from bench.forms import BenchForm
+from bench.forms import BenchForm, ManualForm
 from django.views.generic import FormView
 from samples.forms import SamplesForm
 from app.models import Sample
@@ -60,8 +60,9 @@ class StartSample(FormView):
         # It should return an HttpResponse.
         form.clean()
         query_id, call = form.start_query()
-        self.success_url = SUB_SITE+"/bench/" + query_id
-        os.system(call)
+        # self.success_url = SUB_SITE+"/bench/" + query_id
+        self.success_url = SUB_SITE+"/bench/" + "pick/" + query_id
+        #os.system(call)
 
         return super(StartSample, self).form_valid(form)
 
@@ -122,6 +123,107 @@ class BenchSample(FormView):
         print(call)
         os.system(call)
         return super(BenchSample, self).form_valid(form)
+
+class PickBench(FormView):
+    template_name = 'app/samples_pick.html'
+    form_class = ManualForm
+
+    # # def post(self, request, *args, **kwargs):
+    # #     # request.POST._mutable = True
+    # #     # #print(SPECIES_PATH)
+    # #     # request.POST['species'] = request.POST['species_hidden'].split(',')
+    # #     # print(request.POST['species'])
+    # #     # print(request.POST['species_hidden'].split(','))
+    # #     # request.POST._mutable = False
+    # #
+    # #     post_dict = request.POST
+    # #     import json
+    # #     with open('/opt/liqDB/liqDB/gentelella/queryData/post.json', 'w') as fp:
+    # #         json.dump(post_dict, fp)
+    #
+    #
+    #     return super(StartSample, self).post(request, *args, **kwargs)
+
+
+    def get_context_data(self, **kwargs):
+        context = super(FormView, self).get_context_data(**kwargs)
+        query_id = str(self.request.path_info).split("/")[-1]
+        # content_folder = os.path.join(MEDIA_ROOT, query_id, "queryOutput")
+        content_folder = os.path.join(DATA_FOLDER, "queryData", query_id, "queryOutput")
+        with open(os.path.join(DATA_FOLDER, "queryData", query_id, "query.txt"), 'r') as queryfile:
+            SRX_string = queryfile.read()
+        samples_ids = SRX_string.split(",")
+        samples_ids = list(filter(None, samples_ids))
+        context['pagetitle'] = str(len(samples_ids)) + ' samples selected'
+        samples = Sample.objects.all().filter(Experiment__in=samples_ids)
+        table_data = []
+        for sam in samples:
+            organism = sam.Organism
+            SRP = sam.SRP
+            SRX = sam.Experiment
+            Library = sam.Library
+            BIOS = sam.Sample
+            instrument = sam.Instrument
+            sex = sam.Sex
+            fluid = sam.Fluid
+            extraction = sam.Extraction
+            healthy = sam.Healthy
+            cancer = sam.Cancer
+            exosome = sam.Exosome
+            checkbox = "<input type='checkbox' value='" + sam.Experiment + "' name='to_list'>"
+            desc = sam.Desc
+            table_data.append(
+                #[SRP,SRX, BIOS, organism, instrument, sex, fluid, extraction, Library, healthy, cancer, exosome, desc])
+                #[checkbox,SRP,SRX, BIOS, instrument, sex, fluid, extraction, Library, healthy, cancer, exosome, desc])
+                [checkbox,SRP,SRX, instrument, sex, fluid, extraction, Library, healthy, cancer, exosome, desc])
+        js_data = json.dumps(table_data)
+        #print(js_data)
+        context["data"] = js_data
+
+        return context
+
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+
+        form.clean()
+        old_query = str(self.request.path_info).split("/")[-1]
+        query_id, call, success_url = form.start_query(old_query)
+        self.success_url = success_url
+        #self.success_url = "/samples/" + query_id
+        #success_url = reverse_lazy("mirconstarget")
+        os.system(call)
+
+        return super(PickSample, self).form_valid(form)
+    #success_url = reverse_lazy("BENCH")
+
+    #return super(StartSample, self).form_valid(form)
+    # def post(self, request, *args, **kwargs):
+    #     request.POST._mutable = True
+    #     #print(SPECIES_PATH)
+    #     request.POST['species'] = request.POST['species_hidden'].split(',')
+    #     print(request.POST['species'])
+    #     print(request.POST['species_hidden'].split(','))
+    #     request.POST._mutable = False
+    #     return super(Bench, self).post(request, *args, **kwargs)
+    #
+    # def form_valid(self, form):
+    #     # This method is called when valid form data has been POSTed.
+    #     # It should return an HttpResponse.
+    #
+    #     form.clean()
+    #
+    #     call, pipeline_id = form.create_call()
+    #     self.success_url = reverse_lazy('srnabench') + '?id=' + pipeline_id
+    #
+    #     print(call)
+    #     os.system(call)
+    #     js = JobStatus.objects.get(pipeline_key=pipeline_id)
+    #     js.status.create(status_progress='sent_to_queue')
+    #     js.job_status = 'sent_to_queue'
+    #     js.save()
+    #     return super(StartSample, self).form_valid(form)
+
 
 class BenchCompare(FormView):
     template_name = 'app/samples_query.html'
