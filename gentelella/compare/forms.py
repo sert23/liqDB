8b20e250af35455b748964367c61ce9f6c1380fc
eaparicio45 <erapariciopuerta@hotmail.com>
2018-09-26 11:02:12 +0200

    adding all buttons

--- gentelella/compare/forms.py
+++ gentelella/compare/forms.py
@@ -248,6 +248,7 @@ class CompareForm(forms.Form):
 class ManualForm(forms.Form):
 
     hiddenIDs = forms.CharField(label='', required=False, widget=forms.HiddenInput, max_length=1000000)
+    hiddenIDs2 = forms.CharField(label='', required=False, widget=forms.HiddenInput, max_length=1000000)
 
     #field2=  forms.CharField(label=')', required=False)
 
@@ -257,18 +258,34 @@ class ManualForm(forms.Form):
         self.helper = FormHelper()
         self.helper.layout = Layout(
             Div(
-                Field('hiddenIDs', name='hiddenIDs')),
+                Field('hiddenIDs', name='hiddenIDs'),
+                Field('hiddenIDs2', name='hiddenIDs'),
+                                                     ),
                 #Field('library', wrapper_class='col-md-2',css_class='form-control'),
                 FormActions(
                 # Submit('submit', 'RUN', css_class='btn btn-primary', onclick="alert('Neat!'); return true")
                     #Submit('submit', 'KEEP SELECTED', onclick="$('#loadpage').show(); $('#divPageContent').hide();", css_class='btn btn-primary btn-form')
-                    Submit('submit', 'KEEP SELECTED SAMPLES', onclick = "keepSelected()", css_class='btn btn-primary btn-form'),
+                    Submit('submit', 'KEEP SELECTED (GROUP 1)', onclick = "keepSelected()", css_class='btn btn-primary btn-form'),
 
-                    Submit('submit', 'REMOVE SELECTED SAMPLES', onclick="removeSelected()", css_class='btn btn-primary btn-form'),
+                    Submit('submit', 'REMOVE SELECTED (GROUP 1)', onclick="removeSelected()", css_class='btn btn-primary btn-form'),
 
-                    Submit('submit', 'PROCEED WITH ALL SAMPLES', onclick="$('#loadpage').show(); $('#divPageContent').hide();proceed()", css_class='btn btn-primary btn-form'),
+                    Submit('submit', 'PROCEED WITH CURRENT SAMPLES', onclick="$('#loadpage').show(); $('#divPageContent').hide();proceed()", css_class='btn btn-primary btn-form'),
 
-                )
+                ),
+            FormActions(
+                # Submit('submit', 'RUN', css_class='btn btn-primary', onclick="alert('Neat!'); return true")
+                # Submit('submit', 'KEEP SELECTED', onclick="$('#loadpage').show(); $('#divPageContent').hide();", css_class='btn btn-primary btn-form')
+                Submit('submit', 'KEEP SELECTED (GROUP 2)', onclick="keepSelected()",
+                       css_class='btn btn-primary btn-form'),
+
+                Submit('submit', 'REMOVE SELECTED (GROUP 2)', onclick="removeSelected()",
+                       css_class='btn btn-primary btn-form'),
+
+                Submit('submit', 'PROCEED WITH CURRENT SAMPLES',
+                       onclick="$('#loadpage').show(); $('#divPageContent').hide();proceed()",
+                       css_class='btn btn-primary btn-form'),
+
+            )
 
         )
 
