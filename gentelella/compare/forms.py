22aaae3e4b3809a130ff2949984845d79c629353
eaparicio45 <erapariciopuerta@hotmail.com>
2018-09-26 11:46:35 +0200

    adding link for compare 2 datasets

--- gentelella/compare/forms.py
+++ gentelella/compare/forms.py
@@ -276,7 +276,7 @@ class ManualForm(forms.Form):
             FormActions(
                 # Submit('submit', 'RUN', css_class='btn btn-primary', onclick="alert('Neat!'); return true")
                 # Submit('submit', 'KEEP SELECTED', onclick="$('#loadpage').show(); $('#divPageContent').hide();", css_class='btn btn-primary btn-form')
-                Submit('submit', 'REMOVE SELECTED (GROUP 1)', onclick="keepSelected()",
+                Submit('submit', 'REMOVE SELECT. (GROUP 1)', onclick="keepSelected()",
                        css_class='btn btn-primary btn-form'),
 
                 Submit('submit', 'REMOVE SELECTED (GROUP 2)', onclick="removeSelected()",
