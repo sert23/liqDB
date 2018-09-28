caac315eacb64938ac29a909b52497ba35b128b1
eaparicio45 <erapariciopuerta@hotmail.com>
2018-09-28 16:06:26 +0200

    redire

--- gentelella/app/views.py
+++ gentelella/app/views.py
@@ -225,7 +225,7 @@ def make_table_div(input_file, title):
         <div class="clearfix"></div>
     '''
     table_string = table_template.format(
-        table_headers= ,
+        table_headers= table_headers,
         table_body = table_body,
         title = title
     )
