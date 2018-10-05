4b322ed762fd27f692e4ba3e69267d305128b6bf
eaparicio45 <erapariciopuerta@hotmail.com>
2018-10-05 18:22:12 +0200

    redire

--- gentelella/app/views.py
+++ gentelella/app/views.py
@@ -15,7 +15,7 @@ import os
 def clean_upload():
     import time
     import shutil
-    numdays = 86400 * 15
+    numdays = 86400 * 2
     now = time.time()
     #make white_list
     white_list=[]
@@ -35,7 +35,7 @@ def clean_upload():
         for dir in to_rem:
 
             timestamp = os.path.getmtime(dir)
-            testfile.write(dir + " " + str(timestamp) +" " + str(now - numdays) + "\n")
+            #testfile.write(dir + " " + str(timestamp) +" " + str(now - numdays) + "\n")
             if now - numdays > timestamp:
                 testfile.write(dir + " " + str(timestamp) + "\n")
                 try:
