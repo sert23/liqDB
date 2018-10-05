cb65323078802f6ddcb37477498402a29fb73873
eaparicio45 <erapariciopuerta@hotmail.com>
2018-10-05 18:12:16 +0200

    redire

--- gentelella/app/views.py
+++ gentelella/app/views.py
@@ -15,7 +15,7 @@ import os
 def clean_upload():
     import time
     import shutil
-    numdays = 86400 * 30
+    numdays = 86400 * 15
     now = time.time()
     #make white_list
     white_list=[]
